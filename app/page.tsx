67a57270fbe9dbd6db61e252728bf1bf1f17f221
sinanptm <muhammedsinan7749@gmail.com>
2025-03-17 16:30:12 +0530

    todo updated

--- app/page.tsx
+++ app/page.tsx
@@ -11,13 +11,15 @@ const HomePage = () => {
       <div className="w-full flex flex-col gap-6 p-6 bg-muted rounded-lg">
         <h2 className="text-2xl font-bold mb-4">TODO List:</h2>
         <ul className="space-y-3 list-disc pl-5">
-          <li>Implement Auth0 for authentication</li>
-          <li>Add ai validation to the ideas when creating </li>
+          <li>Add ai validation to the ideas when creating and editing</li>
           <li>Add ai Enhancements to the fields when creating ideas, eg: title, description, value proposition, etc.</li>
           <li>Implement featured ideas section</li>
           <li>Create &quot;How it works&quot; section</li>
-          <li>Add About us page with the story of the website</li>
+          <li>Add About us page with the story of the website and home page content</li>
           <li>Add comment section to each idea</li>
+          <li>Add share buttons to each idea</li>
+          <li>Add email subscription form to the home page</li>
+          <li>Add user dashboard for managing posted ideas</li>
         </ul>
       </div>
     </Container>
