7bb4302a78cf5e58c23dd6babd14eda9bf8d3758
muhammed sinan <muhammedsinan7749@gmail.com>
2025-03-23 14:06:33 +0530

    suspence added

--- app/(pages)/ideas/page.tsx
+++ app/(pages)/ideas/page.tsx
@@ -2,21 +2,29 @@ import IdeaFilters from "@/components/idea/IdeaFilters";
 import IdeaHeader from "@/components/idea/IdeaHeader";
 import IdeaList from "@/components/idea/IdeaList";
 import Container from "@/components/Container";
-import { memo } from "react";
+import { memo, Suspense } from "react";
+import IdeaCardSkeleton from "@/components/home/IdeaCardSkeleton";
+import IdeaFilterSkeleton from "@/components/skeleton/IdeaFilterSkeleton";
 
 const IdeasPage = async () => {
     return (
         <Container>
             <div className="px-4 sm:px-6 lg:px-8 py-8">
-                <IdeaHeader />
+                <Suspense fallback={<IdeaCardSkeleton />}>
+                    <IdeaHeader />
+                </Suspense>
 
                 <div className="mt-8 grid grid-cols-1 lg:grid-cols-4 gap-8">
                     <div className="lg:col-span-1">
-                        <IdeaFilters />
+                        <Suspense fallback={<IdeaFilterSkeleton />}>
+                            <IdeaFilters />
+                        </Suspense>
                     </div>
 
                     <div className="lg:col-span-3">
-                        <IdeaList />
+                        <Suspense fallback={<IdeaCardSkeleton />}>
+                            <IdeaList />
+                        </Suspense>
                     </div>
                 </div>
             </div>
