f6bd3014560c0e17a3a790e1a44a5af6303a12ac
muhammed sinan <muhammedsinan7749@gmail.com>
2025-03-24 11:14:38 +0530

    Logo updated

--- components/logo/Logo.tsx
+++ components/logo/Logo.tsx
@@ -13,8 +13,11 @@ const Logo = () => {
             <div className="flex aspect-square items-center justify-center rounded-lg">
                 <LogoIcon className="w-8 h-8" />
             </div>
-            <div className="text-left text-sm leading-tight">
+            <div className="text-left text-sm leading-tight flex items-center space-x-1">
                 <LogoName />
+                <span className="bg-yellow-200 text-yellow-800 text-xs font-semibold ml-1 px-2 py-0.5 rounded-full">
+                    Beta
+                </span>
             </div>
         </Link>
     );
