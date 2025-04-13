6c9d758bdb2a27d1d8162dbdea47e7109f2464fb
muhammed sinan <muhammedsinan7749@gmail.com>
2025-04-13 22:20:43 +0530

    adds script added

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -3,6 +3,7 @@ import { RootLayoutProps } from "@/types/props";
 import { Fira_Code } from "next/font/google";
 import "../styles/globals.css";
 import { memo } from "react";
+import Script from "next/script";
 const firaCode = Fira_Code({ subsets: ['latin'] });
 
 export { metadata } from "./metadata";
@@ -10,6 +11,12 @@ export { metadata } from "./metadata";
 const RootLayout = ({ children }: RootLayoutProps) => {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4533942133041461"
+          strategy="afterInteractive"
+        />
+      </head>
       <body
         className={`${firaCode.className} antialiased`}
       >
